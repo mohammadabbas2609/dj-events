7c1c5b7752823e40a55db45460c7f4f0c20b6ca2
mohammadabbas2609 <mohammadabbas2609@gmail.com>
2021-08-25 14:31:01 +0530

    getting ready for deploy

--- server/server.js
+++ server/server.js
@@ -26,6 +26,13 @@ app.use(cookie());
 app.use("/api/user", UserRouter);
 app.use("/api/event", EventRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
